d3e823cd7c40e139ead8fb0a54e8ab2598b083e7
thatguygarv <garvvaidya24@outlook.com>
2025-05-14 17:29:41 +0530

    Refactored files into module folders for better structure

---
+++ 01_Basics/Loops - For and While Example.py
@@ -0,0 +1,9 @@
+# For loop
+for i in range(1, 6):
+    print(f"Day {i} of study")
+
+# While loop
+count = 0
+while count < 3:
+    print("Still learning...")
+    count += 1
--- Code Tester.py
+++
@@ -1,5 +0,0 @@
-import pyinputplus as pip
-
-print("Choose a number : ")
-number = pip.inputFloat()
-print(number)
